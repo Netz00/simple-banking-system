62ac02eedba30a7c754b41dcfedd1fa4090c2b5e
Netz <bozo2000d@gmail.com>
2024-08-17 01:02:25 +0200

    Initialize Spring project

---
+++ src/test/java/org/netz00/simple_banking_system/SimpleBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.netz00.simple_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

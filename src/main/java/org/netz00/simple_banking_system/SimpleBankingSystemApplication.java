09f4be23ff1ae5b27162142e81e430826f53e51e
Netz <bozo2000d@gmail.com>
2024-08-17 01:02:25 +0200

    Initialize Spring project

---
+++ src/main/java/org/netz00/simple_banking_system/SimpleBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.netz00.simple_banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankingSystemApplication.class, args);
+	}
+
+}

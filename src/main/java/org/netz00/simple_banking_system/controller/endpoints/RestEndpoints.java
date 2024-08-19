1d76c47f74153ea6a5725cafb8821b2d0b8051cb
Netz <bozo2000d@gmail.com>
2024-08-19 15:19:28 +0200

    Define endpoints

--- src/main/java/org/netz00/simple_banking_system/controller/endpoints/RestEndpoints.java
+++ src/main/java/org/netz00/simple_banking_system/controller/endpoints/RestEndpoints.java
@@ -2,8 +2,9 @@ package org.netz00.simple_banking_system.controller.endpoints;
 
 public class RestEndpoints {
 
-    // TODO: recommend to client using following endpoints instead: v1/api/customers/{id}, v1/api/transactions
+    // TODO: recommend to client using following endpoints instead: v1/api/customers/{id}, v1/api/transactions, v1/api/customers/{id}/transactions
     public static final String API_CUSTOMER = "customer";
     public static final String API_TRANSACTION = "transaction";
+    public static final String API_CUSTOMER_HISTORY = "/history";
 
 }

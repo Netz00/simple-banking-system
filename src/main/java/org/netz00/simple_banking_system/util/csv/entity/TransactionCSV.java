8d20781b9a35eed61febe7a1e7c5497d200d9add
Netz <bozo2000d@gmail.com>
2024-08-23 16:52:27 +0200

    Fix loading transaction date from CSV file

--- src/main/java/org/netz00/simple_banking_system/util/csv/entity/TransactionCSV.java
+++ src/main/java/org/netz00/simple_banking_system/util/csv/entity/TransactionCSV.java
@@ -2,13 +2,16 @@ package org.netz00.simple_banking_system.util.csv.entity;
 
 
 import com.opencsv.bean.CsvBindByName;
-import lombok.Builder;
-import lombok.Getter;
+import com.opencsv.bean.CsvDate;
+import lombok.*;
 
 import java.util.Date;
 
 @Builder
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionCSV {
 
     @CsvBindByName
@@ -21,6 +24,7 @@ public class TransactionCSV {
     private String message;
 
     @CsvBindByName
+    @CsvDate("yyyy-MM-dd HH:mm:ss")
     private Date dateCreated;
 
     @CsvBindByName
